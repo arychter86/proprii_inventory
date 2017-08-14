7769debc6726dfb7d962ccf9946f43cb936adeec
arychter <arychter86@gmail.com>
2017-08-14 17:35:48 +0200

    multitouch map.js

--- static/js/map.js
+++ static/js/map.js
@@ -77,9 +77,9 @@
 			var handleTouchStart=  function(evt) {
 			  // Handle zoom only if 2 fingers are touching the screen
 
-				window.alert('start ' +  evt.touches.length);
-				if ( evt.touches.length == 2) {
 
+				if ( evt.touches.length == 2) {
+				
 				 // Get event point
 				 distStart =Math.sqrt((evt.touches[0].x-evt.touches[1].x) * (evt.touches[0].x-evt.touches[1].x) +(evt.touches[0].y-evt.touches[1].y) * (evt.touches[0].y-evt.touches[1].y));
 				 // Calculate delta from start scale
@@ -89,9 +89,9 @@
 			var handleTouchEnd=  function(evt) {
 			  // Handle zoom only if 2 fingers are touching the screen
 
-				window.alert('stop ' +  evt.touches.length);
-				if ( evt.touches.length == 2) {
 
+				if ( evt.touches.length == 2) {
+					window.alert('stop ' +  evt.touches.length);
 				 // Get event point
 				 var distStop =Math.sqrt((evt.touches[0].x-evt.touches[1].x) * (evt.touches[0].x-evt.touches[1].x) +(evt.touches[0].y-evt.touches[1].y) * (evt.touches[0].y-evt.touches[1].y));
 				 delta = distStop-distStart;
